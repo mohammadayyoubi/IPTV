635b3db1b6bcdadd573810e494645e3e013c5938
Mahmoud Al Natour <natouemanoud@gmail.com>
2025-05-31 15:12:32 +0300

    userside ongoing udates v2 corrct version

---
+++ app/src/main/java/com/example/iptv/activities/user/MainUserActivity.java
@@ -0,0 +1,54 @@
+package com.example.iptv.activities.user;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.example.iptv.Fragments.user.AllChannelsFragment;
+import com.example.iptv.Fragments.user.CategoriesFragment;
+import com.example.iptv.Fragments.user.CountriesFragment;
+import com.example.iptv.Fragments.user.FavoritesFragment;
+import com.example.iptv.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainUserActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_user);
+
+        bottomNavView = findViewById(R.id.bottom_nav_view);
+
+        // Default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new AllChannelsFragment())
+                    .commit();
+            bottomNavView.setSelectedItemId(R.id.nav_all_channels);
+        }
+
+        bottomNavView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            if (item.getItemId() == R.id.nav_all_channels) {
+                selectedFragment = new AllChannelsFragment();
+            } else if (item.getItemId() == R.id.nav_categories) {
+                selectedFragment = new CategoriesFragment();
+            } else if (item.getItemId() == R.id.nav_countries) {
+                selectedFragment = new CountriesFragment();
+            } else if (item.getItemId() == R.id.nav_favorites) {
+                selectedFragment = new FavoritesFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+
+            return true;
+        });
+    }
+}
